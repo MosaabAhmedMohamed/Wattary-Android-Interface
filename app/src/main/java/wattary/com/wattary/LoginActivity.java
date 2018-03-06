63fbe5de43566df8cbf0700cd3c07ca29b543d79
Mosaab Ahmed Mohamed <mosabahmeddev@gmail.com>
2018-03-06 21:46:55 +0200

    make the Camera working in higher functionality

--- app/src/main/java/wattary/com/wattary/LoginActivity.java
+++ app/src/main/java/wattary/com/wattary/LoginActivity.java
@@ -3,62 +3,53 @@ package wattary.com.wattary;
 /**
  * Created by mosabahmed55 on 03/03/2018.
  */
-import android.app.Activity;
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
-    public static final String ALLOW_KEY = "ALLOWED";
-    public static final String CAMERA_PREF = "camera_pref";
+
+static final int REQUEST_iMAGE_CAPTURE=1;
+ImageView MyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button Capture=(Button)findViewById(R.id.CaptureLoginBu);
+         MyView=(ImageView)findViewById(R.id.MyLoginImage);
 
-            openCamera();
-    }
-    public static void saveToPreferences(Context context, String key, Boolean allowed) {
-        SharedPreferences myPrefs = context.getSharedPreferences(CAMERA_PREF,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor prefsEditor = myPrefs.edit();
-        prefsEditor.putBoolean(key, allowed);
-        prefsEditor.commit();
+        if(!hasCamera())
+        {Capture.setEnabled(false);}
+
     }
 
-    public static Boolean getFromPref(Context context, String key) {
-        SharedPreferences myPrefs = context.getSharedPreferences(CAMERA_PREF,
-                Context.MODE_PRIVATE);
-        return (myPrefs.getBoolean(key, false));
+    public Boolean hasCamera()
+    {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    public void CaptuerLogin(View view)
+    {
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+       startActivityForResult(intent, REQUEST_iMAGE_CAPTURE);
+
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    public static void startInstalledAppDetailsActivity(final Activity context) {
-        if (context == null) {
-            return;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==REQUEST_iMAGE_CAPTURE &&resultCode ==RESULT_OK)
+        {
+            Bundle extras =data.getExtras();
+            Bitmap photo =(Bitmap) extras.get("data");
+            MyView.setImageBitmap(photo);
         }
-
-        final Intent i = new Intent();
-        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        i.addCategory(Intent.CATEGORY_DEFAULT);
-        i.setData(Uri.parse("package:" + context.getPackageName()));
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        context.startActivity(i);
-    }
-
-    private void openCamera() {
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
     }
 }
